d5b9a27fcf4cb2b75741183cb435fe57042936a7
Mauricio Romero <mauricionromero@hotmail.com>
2021-09-03 17:00:20 -0300

    added environment variables

--- routes/trivia.js
+++ routes/trivia.js
@@ -1,12 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const axios = require('axios');
+const axios = require("axios");
 
-
-router.get('/', async (req, res) => {
-  response = await axios.get('https://opentdb.com/api.php?amount=10');
+router.get("/", async (req, res) => {
+  response = await axios.get("https://opentdb.com/api.php?amount=10");
   trivia = response.data.results;
   res.json(trivia);
+});
+
+router.get("/img/:term", async (req, res) => {
+  const searchTerm = req.params.term;
+  const options = {
+    method: "GET",
+    url: "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI",
+    params: {
+      q: searchTerm,
+      pageNumber: "1",
+      pageSize: "10",
+      autoCorrect: "true",
+    },
+    headers: {
+      "x-rapidapi-host": process.env.X_HOST,
+      "x-rapidapi-key": process.env.X_KEY,
+    },
+  };
+  const response = await axios.request(options);
+  const images = response.data;
+  res.json(images);
 });
 
 module.exports = router;
